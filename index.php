ed2f39b91fbaaf68a87dd3c87be6c0d186b67588
tipu1992 <117255997+tipu1992@users.noreply.github.com>
2026-05-16 14:36:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,68 +1,105 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require_once('inc/config.php');
-$frame = 0; if(isset($_GET["frame"])) $frame = $_GET["frame"];
-?>
+//------------------------------------------------------------------------------------------------------------+
+  $time = microtime(true);
+  header("Content-Type:text/html; charset=utf-8");
 
+  require ("lgsl_files/lgsl_config.php");
+  global $output, $title;
+
+  function load_page($file) {
+    global $lgsl_config;
+    if ($lgsl_config['preloader']) {
+      $loader = @file_get_contents('lgsl_files/other/loader.html');
+      $post = http_build_query($_POST);
+      $get = http_build_query($_GET);
+      return "
+      <script>
+        (function() {
+          httpRequest = new XMLHttpRequest();
+          if (!httpRequest) {
+            alert('Cannot create an XMLHTTP instance');
+            return false;
+          }
+          httpRequest.onreadystatechange = alertContents;
+          httpRequest.open('POST', 'lgsl_files/lgsl_{$file}.php?{$get}', true);
+          httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+          httpRequest.send('{$post}')
+
+          function alertContents() {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+              if (httpRequest.status === 200) {
+                document.getElementById('container').innerHTML = httpRequest.responseText;
+                if (document.querySelector('[id^=servername]')) {
+                  document.title = document.title + ' | ' + document.querySelector('[id^=servername]').innerText;
+                }
+                window.document.dispatchEvent(new Event('DOMContentLoaded', {
+                  bubbles: true,
+                  cancelable: true
+                }));
+              } else {
+                alert('There was a problem with the request.');
+              }
+            }
+          }
+        })();
+      </script>
+      {$loader}";
+    }
+    else {
+      global $lgsl_server_id;
+      $lgsl_server_id = isset($_GET['s']) ? $_GET['s'] : "";
+      require("lgsl_files/lgsl_{$file}.php");
+      return $output;
+    }
+  }
+
+  $title = $lgsl_config['text']['ttl'];
+  $s = isset($_GET['s']) ? $_GET['s'] : null;
+  $ip = isset($_GET['ip']) ? $_GET['ip'] : null;
+  $port = isset($_GET['port']) ? $_GET['port'] : null;
+  if     (is_numeric($s)) { $output = load_page("details"); }    
+  elseif (isset($ip) && isset($port)) { $output = load_page("details");                                              }
+  elseif ($s === "add")   { $output = load_page("add");     $title .= " | {$lgsl_config["text"]["aas"]}"; }
+  else                    { $output = load_page("list");                                                  }
+//------------------------------------------------------------------------------------------------------------+
+?>
 <!DOCTYPE html>
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href='css/main.css' />
 <html>
   <head>
-    <title><?= $main_name ?></title>
+    <title><?php echo $title; ?></title>
+    <link rel="icon" href="lgsl_files/other/favicon.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="lgsl_files/other/favicon.ico" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+    <meta http-equiv='content-style-type' content='text/css' />
+    <link rel='stylesheet' href='lgsl_files/styles/<?php echo $lgsl_config['style']; ?>' type='text/css' />
+    <?php echo ($lgsl_config['autoreload'] ? "<META HTTP-EQUIV='REFRESH' CONTENT=" . ($lgsl_config['cache_time'] + 1) . " />" : ""); ?>
   </head>
+
   <body>
-    <div id="hd">
-      <img src='css/img/site/statistics.png' style="float: left; margin-right: 10px; width:30px;">
-      <h2><a href="<?=$main_url?>"><?=$main_name?></a>
-    <a href="<?=$main_site?>"><button class="button">Back to site</button></a>
-  </div>
-  <?php
-  if(!$frame) {
-      echo '
-    <table>
-      <tr id=a>
-        <th>Status</th>
-        <th>Name</th>
-        <th>IP</th>
-        <th>Players</th>
-        <th>Map</th>
-      </tr>';
+    <div id="topmenu">
+      <?php
+                                        echo "<li><a href='../../'>{$lgsl_config['text']['mpg']}</a></li>";   // MAIN PAGE
+        if ($lgsl_config['public_add']) echo "<li><a href='?s=add'>{$lgsl_config['text']['aas']}</a></li>";   // ADD SERVER
+        if (file_exists("install.php")) echo "<li><a href='./install.php'>INSTALLATION PAGE</a></li>";        // INSTALLATION PAGE  
+        if (isset($_GET['s']))          echo "<li><a href='./'>{$lgsl_config['text']['bak']}</a></li>";       // BACK TO SERVERS LIST
+      ?>
+    </div>
+    <a id="adminlink" href="admin.php"></a>
 
-      require_once('lgsl/lgsl_files/lgsl_class.php');
-      foreach ($servers as $server) {
-        $server_ip = $server['ip'];
-        $mysql_table1[$server_ip] = $server['mysql_table1'];
-        $mysql_table2[$server_ip] = $server['mysql_table2'];
-
-        $result = lgsl_query_live('halflife', $server_ip, $server['port'], $server['port'], 0, "sep");
-        if($result['b']['status']) {
-            $players = $result['s']['players'] . '/' . $result['s']['playersmax'];
-            $map_image = '<img class="map-image" src="https://image.gametracker.com/images/maps/160x120/cs/' . strtolower($result['s']['map']) . '.jpg" alt="' . $result['s']['map'] . '" />';
-            $server_name = $result['s']['name'];
-            echo '<tr>';
-            echo '<td id=on></td>';
-            echo '<td><a href='.$main_url.'?frame=1&db_table1='.$mysql_table1[$server_ip].'&db_table2='.$mysql_table2[$server_ip].'>' . $server_name . '</a></td>';
-            echo '<td>' . $server_ip . ':' . $server['port'] . '</td>';
-            echo '<td>' . $players . '</td>';
-            echo '<td>' . $map_image . '</td>';
-            echo '</tr>';
-        } else {
-            $server_name = $server_ip . ':' . $server['port'];
-            echo '<tr>';
-            echo '<td id=off></td>';
-            echo '<td>' . $server_name . '</td>';
-            echo '<td>' . $server_ip . ':' . $server['port'] . '</td>';
-            echo '<td>N/A</td>';
-            echo '<td>N/A</td>';
-            echo '</tr>';
+    <div id="container">
+      <?php
+        echo $output;
+        unset($output);
+      ?>
+    </div>
+    <?php
+      if (isset($lgsl_config['scripts'])) {
+        foreach ($lgsl_config['scripts'] as $script) {
+          echo "<script src='lgsl_files/scripts/{$script}'></script>";
         }
-    }
-      echo '</table>';
-    } else {
-      echo '<iframe src='.$main_url.'top15.php?top=15&player=&style=1&order='.$default_order.'&default_order='.$default_order.'&db_table1='.htmlspecialchars($_GET['db_table1']).'&db_table2='.htmlspecialchars($_GET['db_table2']).'&search=';
-    }
-  ?>
+      }
+    ?>
   </body>
 </html>
+<!-- Powered by LGSL v6.2.0; <?php echo "Page loaded: ".round(microtime(true) - $time, 6)."s";?> -->
